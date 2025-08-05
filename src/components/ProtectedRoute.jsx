673def974da53512752e4a406a2ce02ce54fbf74
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-08-05 18:14:58 -0400

    Delete src directory

--- src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/ProtectedRoute.jsx
-import { Navigate } from "react-router-dom";
-import { useAuth } from "./AuthContext.jsx";
-
-export default function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useAuth();
-
-  return isAuthenticated ? children : <Navigate to="/" replace />;
-}
